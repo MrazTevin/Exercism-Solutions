2bc52900aaae2df6e07fd9a5302d6f6d3345e837
MrazTevin <millatevin@gmail.com>
2024-03-06 21:13:24 +0300

    m: solve for cook lasagna

--- lasagna/src/main/java/Lasagna.java
+++ lasagna/src/main/java/Lasagna.java
@@ -1,9 +1,22 @@
 public class Lasagna {
+
+    // int minutesInOven = 30;
     // TODO: define the 'expectedMinutesInOven()' method
+    public int expectedMinutesInOven() {
+        return 40;
+    }
 
     // TODO: define the 'remainingMinutesInOven()' method
-
+    public int remainingMinutesInOven(int minutesInOven) {
+        return expectedMinutesInOven() - minutesInOven;
+    }
     // TODO: define the 'preparationTimeInMinutes()' method
+    public int preparationTimeInMinutes(int layers) {
+        return layers * 2;
+    }
 
     // TODO: define the 'totalTimeInMinutes()' method
+    public int totalTimeInMinutes(int layers, int minutesInOven) {
+        return preparationTimeInMinutes(layers) + minutesInOven;
+    }
 }
